e97c6426084c8c7e664b960956bb60c212017efa
JBKIM <drv98@naver.com>
2024-04-19 09:46:50 +0900

    4월 19일 수업

--- src/main/java/org/zerock/w2/controller/LoginController.java
+++ src/main/java/org/zerock/w2/controller/LoginController.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/login")
@@ -17,5 +18,19 @@ public class LoginController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         log.info("Login doGet....");
         req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //mid , mpw 받기
+        String mid = req.getParameter("mid");
+        String mpw = req.getParameter("mpw");
+
+        String str = mid + mpw;
+        HttpSession session = req.getSession();
+        //세션(서버)에 아이디+패스워드해서 문자열로 저장(loginInfo)
+        session.setAttribute("loginInfo", str);
+
+        resp.sendRedirect("/todo/list");
     }
 }
--- src/main/webapp/WEB-INF/login.jsp
+++ src/main/webapp/WEB-INF/login.jsp
@@ -12,5 +12,10 @@
 </head>
 <body>
     <h1>로그인 페이지</h1>
+    <form action="/login" method="post">
+        <input type="text" name="mid" placeholder="아이디">
+        <input type="text" name="mpw" placeholder="비번">
+        <button type="submit">로그인</button>
+    </form>
 </body>
 </html>
